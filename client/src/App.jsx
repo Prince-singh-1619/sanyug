f427b55abdc5a54a0cd45d83d86092ced53c3648
Prince Singh <Princesinghps1619@gmail.com>
2025-07-05 12:55:18 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='root'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil natus, porro ab impedit doloribus, placeat laborum ut tenetur vitae dignissimos deserunt magni et reprehenderit ducimus doloremque rem ratione? Numquam consequatur debitis similique velit! Aspernatur fuga, aliquid autem consectetur officia a maiores eum, totam cum adipisci ex! Error, libero eligendi magnam incidunt dolor eos, doloremque cum numquam ipsam nulla unde nisi, laboriosam explicabo necessitatibus? Eius, iure dignissimos! Sint placeat non dolor quasi, facilis excepturi commodi beatae nostrum eum illo vitae doloremque fuga iure sapiente enim est repellendus, aspernatur laudantium aperiam totam at. Voluptatem obcaecati placeat, accusantium voluptates sit praesentium dolor id.</p>
+    </div> 
+  )
+}
+
+export default App
